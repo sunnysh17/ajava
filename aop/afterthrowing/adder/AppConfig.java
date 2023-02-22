39ccb5d652913289ee1a8be874019b2c17324104
sunnysh17 <123319227+sunnysh17@users.noreply.github.com>
2023-02-23 01:40:06 +0530

    Create AppConfig.java

---
+++ aop/afterthrowing/adder/AppConfig.java
@@ -0,0 +1,12 @@
+package com.sunny.aop3;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@ComponentScan(basePackages = "com.sunny.aop3")
+public class AppConfig {
+	
+}
