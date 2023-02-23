4a85777c088e994c00552edeb1d06bf811a50c4d
sunnysh17 <123319227+sunnysh17@users.noreply.github.com>
2023-02-23 08:32:46 +0530

    6_Application

---
+++ rest/Application.java
@@ -0,0 +1,12 @@
+package com.sunny.restpracticals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
