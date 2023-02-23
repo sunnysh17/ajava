8f80237c68dd9e044ae3426990a28c51abbec7eb
sunnysh17 <123319227+sunnysh17@users.noreply.github.com>
2023-02-23 08:32:07 +0530

    5_ErrorController

---
+++ rest/CustomErrorController.java
@@ -0,0 +1,21 @@
+package com.sunny.restpracticals;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+	@RequestMapping("/error")
+	@ResponseBody
+	  String error(HttpServletRequest request) {
+	    return "<h1>Error occurred</h1>";
+	  }
+	
+	public String getErrorPath() {
+	    return "/error";
+	  }
+}
